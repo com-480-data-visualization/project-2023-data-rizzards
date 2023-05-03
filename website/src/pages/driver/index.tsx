deac46e80355261309f1804ec1ff72f37e43bfc8
Elisa Michalski <elisa.michalski@epfl.ch>
2023-05-03 18:24:35 +0200

    started driver overview

--- website/src/pages/driver/index.tsx
+++ website/src/pages/driver/index.tsx
@@ -1,44 +1,122 @@
 import React from "react";
-import { ResponsiveBump } from "@nivo/bump";
+import {ResponsiveBump} from "@nivo/bump";
+import {useState} from "react";
 import Box from "@mui/joy/Box";
-import { Typography } from "@mui/joy";
+import Button from '@mui/joy/Button';
+import {FormLabel} from "@mui/joy";
+import Autocomplete from "@mui/joy/Autocomplete";
+import {Typography} from "@mui/joy";
 
 const Driver: React.FC = () => {
-  const data = [
-    {
-      id: "Serie 1",
-      data: [
+
+    const [selectedDriver, setSelectedDriver] = useState("");
+
+
+    const drivers = ["Alonso", "Hamiltons"]
+    const driverOptions = drivers
+
+    const handleDriverSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const driver = e.target.value;
+        setSelectedDriver(driver);
+    };
+
+    const handleClick = () => {
+        console.log('The inner html was clicked.');
+        // document.getElementById('output').innerHTML = '';
+    };
+
+    const data = [
         {
-          x: 2000,
-          y: 2,
+            id: "Serie 1",
+            data: [
+                {
+                    x: 2000,
+                    y: 2,
+                },
+                {
+                    x: 2001,
+                    y: 3,
+                },
+                {
+                    x: 2002,
+                    y: 4,
+                },
+            ],
         },
-        {
-          x: 2001,
-          y: 3,
-        },
-        {
-          x: 2002,
-          y: 4,
-        },
-      ],
-    },
-  ];
-  return (
-    <Box
-      display="grid"
-      gridTemplateRows="1fr 5fr"
-      gridTemplateAreas={`"para"
-    "plot"`}
-    >
-      <Box gridArea="para" padding="2em">
-        <Typography level="h2">Driver page</Typography>
-        <Typography level="body1">Lorem ipsum...</Typography>
-      </Box>
-      <Box gridArea="plot" padding="3em" height="360px">
-        <ResponsiveBump data={data} />
-      </Box>
-    </Box>
-  );
+    ];
+
+    return (
+        <Box
+            display="grid"
+            gridTemplateColumns="1fr 4fr"
+            gridTemplateRows="2fr 1fr 4sfr"
+            gridTemplateAreas={`"para para" 
+              "inputs inputs"
+              "list plot"`}
+            gap={4}
+        >
+            <Box gridArea="para" padding="2em">
+                {PageExplanation}
+            </Box>
+
+            <Box
+                gridArea="inputs"
+                display="flex"
+                justifyContent="space-evenly"
+                alignItems="end"
+                id='output'
+            >
+                <Box>
+                    <label>
+                        <FormLabel>Choose your driver:</FormLabel>
+                        <Autocomplete
+                            placeholder="Driver"
+                            options={driverOptions}
+                        />
+                    </label>
+                </Box>
+                <Box>
+                    <Button
+                        variant="outlined"
+                        size='sm'
+                        color='danger'
+                        onClick={handleClick}
+                    >
+                        Reset
+                    </Button>
+                </Box>
+            </Box>
+            <Box
+                gridArea="list"
+                display="flex"
+                justifyContent="column"
+                textAlign="center"
+                sx={{ padding: "5%" }}
+            >
+                list of selected drivers
+            </Box>
+            <Box
+                gridArea="plot"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                padding="3em"
+                height="360px"
+            >
+                <ResponsiveBump data={data}/>
+            </Box>
+        </Box>
+    );
 };
 
 export default Driver;
+
+const PageExplanation = (
+    <>
+        <Typography level="h2" sx={{textAlign: "center"}}>
+            Overview of drivers' seasons
+        </Typography>
+        <br/>
+        <Typography level="body1"> Explain how to use this page </Typography>
+    </>
+);
