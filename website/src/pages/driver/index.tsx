4180a58ff4577f7991126f32b160537c0c42240b
Dixit Sabharwal <dixit.sabharwal@epfl.ch>
2023-05-03 17:15:11 +0200

    add nivo/core and nivo/bump packages

--- website/src/pages/driver/index.tsx
+++ website/src/pages/driver/index.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { ResponsiveBump } from "@nivo/bump";
+import Box from "@mui/joy/Box";
+import { Typography } from "@mui/joy";
 
 const Driver: React.FC = () => {
-  return <div>Driver Page</div>;
+  const data = [
+    {
+      id: "Serie 1",
+      data: [
+        {
+          x: 2000,
+          y: 2,
+        },
+        {
+          x: 2001,
+          y: 3,
+        },
+        {
+          x: 2002,
+          y: 4,
+        },
+      ],
+    },
+  ];
+  return (
+    <Box
+      display="grid"
+      gridTemplateRows="1fr 5fr"
+      gridTemplateAreas={`"para"
+    "plot"`}
+    >
+      <Box gridArea="para" padding="2em">
+        <Typography level="h2">Driver page</Typography>
+        <Typography level="body1">Lorem ipsum...</Typography>
+      </Box>
+      <Box gridArea="plot" padding="3em" height="360px">
+        <ResponsiveBump data={data} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Driver;
