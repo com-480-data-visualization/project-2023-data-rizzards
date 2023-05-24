5cd57719c2fe13aadea3207b217ee68f254bec35
Elisa Michalski <elisa.michalski@epfl.ch>
2023-05-24 12:58:15 +0200

    improved interactivity

--- website/src/pages/driver/index.tsx
+++ website/src/pages/driver/index.tsx
@@ -14,185 +14,178 @@ import {MyResponsiveBump} from "./MyResponsiveBump";
 
 const Driver: React.FC = () => {
 
-    const [selectedDriver, setSelectedDriver] = useState("");
+    const [selectedDrivers, setSelectedDrivers] = useState<string[]>(['Hamilton']);
+    const [selectedYear, setSelectedYear] = useState<number>(2012);
 
-
-    const drivers = {
-        id: "Drivers",
-        data: [
-            {
-                name: "Alonso",
-            },
-            {
-                name: "Hamiltons",
-            },
-        ],
-    };
-    const driverOptions = drivers.data
-
-    const handleDriverSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const driver = e.target.value;
-        setSelectedDriver(driver);
+    const handleDriverSelectChange = (event: React.ChangeEvent<{}>, value: string | null) => {
+        console.log('The driver was selected: ', value);
+        if (value !== null && !selectedDrivers.includes(value)) {
+            setSelectedDrivers([...selectedDrivers, value]);
+        }
+        console.log('The drivers selected are now: ', selectedDrivers);
     };
 
-    const handleClick = () => {
-        console.log('The inner html was clicked.');
-        // document.getElementById('output').innerHTML = '';
+    const handleClickReset = () => {
+        console.log('The Reset button was clicked.');
+        setSelectedDrivers(['Hamilton']);
+        console.log('The drivers selected are now: ', selectedDrivers);
     };
 
-    const data = [
+    function handleClickCheckbox(driver: string) {
+        console.log('The Checkbox of ', driver, ' was clicked.');
+        if (selectedDrivers.length === 1) {
+            console.log('Cannot remove the last driver.');
+            return;
+        }
+        setSelectedDrivers(selectedDrivers.filter((item) => item !== driver));
+        console.log('The drivers selected are now: ', selectedDrivers);
+    };
+
+    const driver_teammates = [
+        {
+            id: "Hamilton",
+            data: [
+                "Alonso"
+            ],
+        },
         {
             id: "Alonso",
             data: [
-                {
-                    x: 2000,
-                    y: 2,
-                },
-                {
-                    x: 2001,
-                    y: 3,
-                },
-                {
-                    x: 2002,
-                    y: 3,
-                },
+                "Hamilton"
+            ],
+        },];
+
+    function handleClickTeammates(driver : string) {
+        console.log('The Add teammates button of ', driver,' was clicked.');
+        const teammates = driver_teammates.find((item) => item.id === driver)?.data || [];
+        console.log('The teammates are ', teammates);
+        for (const value of teammates){
+            if (value !== null && !selectedDrivers.includes(value)) {
+                setSelectedDrivers([...selectedDrivers, value]);
+            }
+        }
+        console.log('The drivers selected are now: ', selectedDrivers);
+    };
+    function addDriverToList(driver : string) {
+        return(
+            <Box>
+                <ListItem>
+                    <ListItemContent>
+                        <Typography level="body1">{driver}</Typography>
+                    </ListItemContent>
+                    <Checkbox
+                        color='danger'
+                        defaultChecked
+                        onClick={() => handleClickCheckbox(driver)}
+                    />
+                </ListItem>
+                <Button
+                    variant="outlined"
+                    size='sm'
+                    color='danger'
+                    onClick={() => handleClickTeammates(driver)}
+                >
+                    Add teammates
+                </Button>
+            </Box>);
+    };
+
+
+
+    const data_rank = [
+        {
+            id: "Alonso",
+            data: [
+                {x: 2000, y: 2},
+                {x: 2001,y: 3},
+                {x: 2002,y: 3},
             ],
         },
         {
             id: "Hamilton",
             data: [
-                {
-                    x: 2000,
-                    y: 1,
-                },
-                {
-                    x: 2001,
-                    y: 2,
-                },
-                {
-                    x: 2002,
-                    y: 1,
-                },
+                {x: 2000,y: 1,},
+                {x: 2001,y: 2,},
+                {x: 2002,y: 1,},
             ],
-        },
-    ];
+        },];
+    const name_list = data_rank.map(item => item.id);
 
-    const years = [
+    const handleYearSelectChange = (event: React.ChangeEvent<{}>, value: number | null) => {
+        console.log('The year was selected: ', value);
+        if (value !== null) {
+            setSelectedYear(value);
+        }
+
+    };
+
+    const data_points = [
         {
-            id: "Alonso",
-            data: [
+            year: 2012,
+            year_data: [
                 {
-                    x: 0,
-                    y: 0,
+                    id: "Alonso",
+                    data: [
+                        {x: 0, y: 0,},
+                        {x: 1, y: 3,},
+                        {x: 2, y: 3,},
+                        {x: 3, y: 5,},
+                        {x: 4, y: 15,},
+                        {x: 5, y: 20,},
+                        {x: 6, y: 25,},
+                        {x: 7, y: 30,},
+                        {x: 8, y: 31,},
+                        {x: 9, y: 35,},
+                        {x: 10, y: 40,},
+                        {x: 11, y: 45,},
+                        {x: 12, y: 50,},
+                    ],
                 },
                 {
-                    x: 1,
-                    y: 3,
-                },
-                {
-                    x: 2,
-                    y: 3,
-                },
-                {
-                    x: 3,
-                    y: 5,
-                },
-                {
-                    x: 4,
-                    y: 15,
-                },
-                {
-                    x: 5,
-                    y: 20,
-                },
-                {
-                    x: 6,
-                    y: 25,
-                },
-                {
-                    x: 7,
-                    y: 30,
-                },
-                {
-                    x: 8,
-                    y: 31,
-                },
-                {
-                    x: 9,
-                    y: 35,
-                },
-                {
-                    x: 10,
-                    y: 40,
-                },
-                {
-                    x: 11,
-                    y: 45,
-                },
-                {
-                    x: 12,
-                    y: 50,
+                    id: "Hamilton",
+                    data: [
+                        {x: 0, y: 0,},
+                        {x: 1, y: 6,},
+                        {x: 2, y: 7,},
+                        {x: 3, y: 10,},
+                        {x: 4, y: 15,},
+                        {x: 5, y: 20,},
+                        {x: 6, y: 21,},
+                        {x: 7, y: 25,},
+                        {x: 8, y: 28,},
+                        {x: 9, y: 30,},
+                        {x: 10, y: 40,},
+                        {x: 11, y: 41,},
+                        {x: 12, y: 42,},
+                    ],
                 },
             ],
         },
         {
-            id: "Hamilton",
-            data: [
+            year: 2013,
+            year_data: [
                 {
-                    x: 0,
-                    y: 0,
-                },
-                {
-                    x: 1,
-                    y: 6,
-                },
-                {
-                    x: 2,
-                    y: 7,
-                },
-                {
-                    x: 3,
-                    y: 10,
-                },
-                {
-                    x: 4,
-                    y: 15,
-                },
-                {
-                    x: 5,
-                    y: 20,
-                },
-                {
-                    x: 6,
-                    y: 21,
-                },
-                {
-                    x: 7,
-                    y: 25,
-                },
-                {
-                    x: 8,
-                    y: 28,
-                },
-                {
-                    x: 9,
-                    y: 30,
-                },
-                {
-                    x: 10,
-                    y: 40,
-                },
-                {
-                    x: 11,
-                    y: 41,
-                },
-                {
-                    x: 12,
-                    y: 42,
+                    id: "Alonso",
+                    data: [
+                        {x: 0,y: 0,},
+                        {x: 1,y: 3,},
+                        {x: 2,y: 3,},
+                        {x: 3,y: 5,},
+                        {x: 4,y: 15,},
+                        {x: 5,y: 20,},
+                        {x: 6,y: 25,},
+                        {x: 7,y: 30,},
+                        {x: 8,y: 31,},
+                        {x: 9,y: 35,},
+                        {x: 10,y: 40,},
+                        {x: 11,y: 45,},
+                        {x: 12,y: 50,},
+                    ],
                 },
             ],
         },
     ];
+    const year_list = data_points.map(item => item.year);
 
     return (
         <Box
@@ -220,7 +213,8 @@ const Driver: React.FC = () => {
                         <FormLabel>Select a driver:</FormLabel>
                         <Autocomplete
                             placeholder="Driver"
-                            options={["Alonso", "Hamilton"]}
+                            options={name_list}
+                            onChange={handleDriverSelectChange}
                         />
                     </label>
                 </Box>
@@ -229,7 +223,7 @@ const Driver: React.FC = () => {
                         variant="outlined"
                         size='sm'
                         color='danger'
-                        onClick={handleClick}
+                        onClick={handleClickReset}
                     >
                         Reset
                     </Button>
@@ -247,25 +241,9 @@ const Driver: React.FC = () => {
                         List of selected drivers
                     </Typography>
                     <List sx={{padding: "5%"}}>
-                        {drivers.data.map(({name}) => (
-                            <Box>
-                                <ListItem>
-                                    <ListItemContent>
-                                        <Typography level="body1">{name}</Typography>
-                                    </ListItemContent>
-                                    <Checkbox color='danger' defaultChecked/>
-                                </ListItem>
-                                <Button
-                                    variant="outlined"
-                                    size='sm'
-                                    color='danger'
-                                    onClick={handleClick}
-                                >
-                                    Add teammates
-                                </Button>
-                            </Box>
+                        {selectedDrivers.map((item) => (
+                            addDriverToList(item)
                         ))}
-
                     </List>
                 </Sheet>
             </Box>
@@ -275,7 +253,8 @@ const Driver: React.FC = () => {
                 padding="3em"
                 height="360px"
             >
-                <MyResponsiveBump data={data}/>
+                <MyResponsiveBump data={data_rank.filter((item)=>
+                    selectedDrivers.includes(item.id))}/>
             </Box>
             <Box
                 gridArea="plot2"
@@ -284,10 +263,21 @@ const Driver: React.FC = () => {
                 padding="3em"
                 height="360px"
             >
-                <Typography level="body1" textTransform="uppercase" fontWeight="lg">
-                    Year: 2019
-                </Typography>
-                <MyResponsiveAreaBump data={years}/>
+                <label>
+                    <FormLabel>Select a year:</FormLabel>
+                    <Autocomplete
+                        placeholder="Year"
+                        options={year_list}
+                        getOptionLabel={(option) => String(option)}
+                        onChange={handleYearSelectChange}
+                    />
+                </label>
+
+                <MyResponsiveAreaBump data={data_points.filter((item) =>
+                    item.year === selectedYear)
+                    [0]?.year_data
+                    .filter((item)=> selectedDrivers.includes(item.id))
+                }/>
             </Box>
         </Box>
     )
